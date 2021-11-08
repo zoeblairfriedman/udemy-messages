c12ba532aca7c6922a7543bc1348961931a6f5e5
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-11-08 14:48:19 -0500

    adds request handles via REST client extension

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,20 @@
+import { Controller , Get, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  
+  @Get()
+  listMessages(){
+
+  }
+
+  @Post()
+  createMessage(){
+
+  }
+
+  @Get('/:id')
+  getMessage(){
+
+  }
+}
