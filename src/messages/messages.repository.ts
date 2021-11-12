e21f76777b68864e97a18d264e935167e41814f3
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-11-12 11:17:27 -0500

    adds messages service

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -9,11 +9,24 @@ export class MessagesRepository {
   }
 
   async findAll() {
+    const contents = await readFile('messages.json', 'utf8')
+    const messages = JSON.parse(contents)
 
+    return messages;
   }
 
-  async create(message: string) {
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8')
+    const messages = JSON.parse(contents)
 
+    const id = Math.floor(Math.random() * 999)
+    
+    messages[id] = {id, content}
+
+    await writeFile('messages.json', JSON.stringify(messages))
   }
 
+
+
+  
 }
